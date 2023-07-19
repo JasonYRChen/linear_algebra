75a51b7f4f95218e1b0421469622dcc87ffd406b
JasonYRChen <jkmylife@gmail.com>
2023-07-19 10:25:08 +0800

    (reduced_row_echelon_form): complete

--- reduced_row_echelon_form.py
+++ reduced_row_echelon_form.py
@@ -2,55 +2,6 @@ import numpy as np
 from elementary_row_operation import *
 
 
-# scaling(matrix, row_index, scalar)
-# row_addition(matrix, from_row, scalar, to_row)
-# interchange(matrix, row1, row2)
-
-"""
-    Beware that row echelon needs non-zero leading element at the front
-    of each column. You may need to check the first non-zero element in
-    each column.
-
-    case1.
-      [[0, 0, 1],
-       [1, 2, 3]]
-      --> [[1, 2, 3],
-           [0, 0, 1]]
-    case2.
-      [[0, 0, 1],
-       [0, 1, 1]]
-      --> [[0, 1, 1],
-           [0, 0, 1]]
-    case3.
-      [[0, 0, 1],
-       [0, 0, 0],
-       [1, 0, 3]]
-      --> [[1, 0, 3],
-           [0, 0, 1],
-           [0, 0, 0]]
-"""
-
-
-def pivot_column(matrix, pivot):
-    """
-        Find the minimum of possible pivot column. The actual column can 
-        only be larger than or equal to the result.
-
-        paras:
-          matrix: np.ndarray
-          pivot: int, the pivot point to find.
-
-        return:
-          column: int, the minimum of possible pivot column.
-    """
-
-    if pivot:
-        column = (matrix[pivot - 1] != 0).argmax() + 1
-    else:
-        column = 0
-    return column
-
-
 def leading_nonzero(matrix, row):
     """
         Return the index of leading non-zero element in the row.
@@ -71,7 +22,8 @@ def leading_nonzero(matrix, row):
 def set_pivot_row(matrix, pivot, column):
     """
         Interchange two rows in the matrix to set pivot row at current
-        pivot position. This is an in-place operation.
+        pivot position. Return the interchanged matrix and next pivot and
+        next column to deal with.
 
         paras:
           matrix: np.ndarray.
@@ -80,6 +32,8 @@ def set_pivot_row(matrix, pivot, column):
 
         return:
           matrix: np.ndarray.
+          next_pivot: int, the next pivot.
+          next_column: int, the next column.
     """
 
     # column = leading_nonzero(matrix, pivot - 1) + 1 if pivot else 0
@@ -92,24 +46,49 @@ def set_pivot_row(matrix, pivot, column):
             continue
         break
 
-    return matrix
+    # determine the next pivot and column
+    column = leading_nonzero(matrix, pivot)
+    if (column == matrix.shape[1] - 1) or (column == -1):
+        column = -2
+    next_pivot = pivot + 1
+    next_column = column + 1
+    return matrix, next_pivot, next_column
 
 
 def reduced_row_echelon_form(matrix):
     """
+        Calculate reduced row echelon form of input matrix.
 
+        para:
+          matrix: np.ndarray
+
+        return:
+          matrix: np.ndarray, the reduced row echelon form of input matrix.
     """
 
-    matrix = np.array(matrix)
+    matrix = np.array(matrix) # copy original matrix
     pivot = 0
     column = 0
-    for _ in range(min(matrix.shape) - 1):
-        matrix = set_pivot_row(matrix, pivot, column)
-        column = leading_nonzero(matrix, pivot)
-        if (column == matrix.shape[1] - 1) or (column == -1):
+
+    for _ in range(min(matrix.shape)):
+        # find and set pivot row
+        matrix, pivot, column = set_pivot_row(matrix, pivot, column)
+
+        # scaling the pivot row by inverse of leading nonzero number 
+        pivot_column = leading_nonzero(matrix, pivot - 1)
+        leading_number = matrix[pivot - 1][pivot_column]
+        scalar = 1 / leading_number if leading_number else 0
+        matrix = scaling(matrix, pivot - 1, scalar)
+
+        # row addition of pivot row and other rows
+        for row in range(matrix.shape[0]):
+            if row != pivot - 1:
+                scalar = -matrix[row][pivot_column]
+                matrix[row] += matrix[pivot - 1] * scalar
+
+        # remaining rows are zeros, no need to calculate
+        if column == -1:
             break
-        pivot += 1
-        column += 1
 
     return matrix
 
@@ -117,14 +96,15 @@ def reduced_row_echelon_form(matrix):
 if __name__ == '__main__':
     from elementary_row_operation import to_ndarray
     a1 = to_ndarray([[r*3+c for c in range(1, 4)] for r in range(0, 4)])
-    a2 = np.array([[0, 0, 1], [1, 2, 3]])
-    a3 = np.array([[0, 0, 1], [0, 1, 1]])
-    a4 = np.array([[0, 0, 1], [0, 0, 0], [1, 0, 3]])
-    a5 = np.array([[0, 0, 1, 0], [0, 0, 0, 0], [1, 0, 1, 1], [0, 1, 1, 1]])
-    a6 = np.array([[0, 1, 0], [1, 0, 0]])
+    a2 = np.array([[0, 0, 3], [2, 2, 3]])
+    a3 = np.array([[0, 0, 1], [0, 3, 9]])
+    a4 = np.array([[0, 0, 7], [0, 0, 0], [3, 0, 3]])
+    a5 = np.array([[0, 0, 11, 0], [0, 0, 0, 0], [2, 0, 8, 6], [0, 7, 7, 14]])
+    a6 = np.array([[0, 9, 0], [5, 0, 0]])
     a7 = np.zeros((3, 3))
     a8 = np.array([[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]])
+    a9 = np.array([[1, 2, -1, 2, 1, 2], [-1, -2, 1, 2, 3, 6], [2, 4, -3, 2, 0, 3], [-3, -6, 2, 0, 3, 9]])
 
-    matrix = a7
+    matrix = a9
     print(matrix)
     print(reduced_row_echelon_form(matrix))
