5773a5bfec944a00b7b225bd7ddcfe78a54fab43
JasonYRChen <jkmylife@gmail.com>
2023-07-26 15:16:19 +0800

    (least_squares): remove least squares matrix and add least squares approximation and error calculation

--- least_squares.py
+++ least_squares.py
@@ -3,57 +3,88 @@ from orthogonal_projection import orthogonal_projection_coefficients as opc
 from orthogonal_projection import orthogonal_projection_matrix_LI as opm
 
 
-def least_squares_coefficients(matrix):
+def least_squares_coefficients(matrix, target_vector):
     """
-        Return a matrix for computing least squares coefficients. If the
-        input matrix is not linearly independent, an empty np.ndarray 
-        returns.
+        Return least squares coefficients. Note that the input matrix will
+        be copied and added a column of 1s vector on the left. If the 
+        matrix is not linearly independent, an empty np.ndarray returns.
 
-        para:
-          matrix: np.ndarray, a matrix with linearly independent columns.
+        paras:
+          matrix: np.ndarray
+          target_vector: np.ndarray, a column vector contains values to be 
+            approximated. This should be n x 1 vector.
 
         return:
-          matrix_for_coef: np.ndarray, matrix for computing coefficients.
+          coefficients: np.ndarray, least squares coefficients.
     """
 
     matrix = np.array(matrix)
     matrix = np.concatenate((np.ones((matrix.shape[0], 1)), matrix), 1)
     matrix_for_coef = opc(matrix)
+    coefficients = np.array([])
+    if matrix_for_coef.any():
+        coefficients = matrix_for_coef @ target_vector
+    return coefficients
 
-    return matrix_for_coef
 
-
-def least_squares_matrix(matrix):
+def least_squares_approximation(matrix, target_vector):
     """
-        Return a matrix to compute least squares approximation. If the
-        input matrix has any column vector linearly dependent of others
-        or all one vector([1...1]), the returned matrix is an empty
-        np.ndarray.
+        Return an approximation vector that approximates a target vector.
+        Note that the input matrix will be copied and added a column of 1s 
+        vector on the left. If the matrix has any column vector linearly
+        dependent of others or all one vector([1...1]), the returned 
+        matrix is an empty np.ndarray.
 
         para:
           matrix: np.ndarray.
+          target_vector: np.ndarray, a column vector contains values to be 
+            approximated. This should be n x 1 vector.
 
         return:
-          matrix_least_squares: np.ndarray, matrix for computing least
-            squares approximation.
+          approximation_vector: np.ndarray, the vector that approximates
+            target_vector.
     """
 
     matrix = np.array(matrix)
     matrix = np.concatenate((np.ones((matrix.shape[0], 1)), matrix), 1)
     matrix_least_squares = opm(matrix)
-    return matrix_least_squares
+    approximation_vector = np.array([])
+    if matrix_least_squares.any():
+        approximation_vector = matrix_least_squares @ target_vector
+    return approximation_vector
+
+
+def least_squares_approximation_error(target_vector, approx_vector):
+    """
+        Return the error of least squares approximation result, which is 
+        the sum of the squared difference between target vector and
+        approximation vector.
+
+        paras:
+          target_vector: np.ndarray, the target vector to be approximated.
+          approx_vector: np.ndarray, the approximation vector.
+
+        return:
+          error: float, the approximation error.
+    """
+
+    error = sum((target_vector - approx_vector) ** 2)
+    return error 
 
 
 if __name__ == '__main__':
     a0 = (np.array([2.6, 2.72, 2.75, 2.67, 2.68])[:, np.newaxis],
           np.array([2.0, 2.1, 2.1, 2.03, 2.04])[:, np.newaxis])
+    a1 = (np.ones((5, 1)),
+          np.array([2.0, 2.1, 2.1, 2.03, 2.04])[:, np.newaxis])
 
-    system = a0
-    ls_coef = least_squares_coefficients(system[0])
+    system = a1
+    ls_coef = least_squares_coefficients(*system)
+    approx_vector = least_squares_approximation(*system)
     print(f'x:\n{system[0]}')
     print(f'least squares coefficients:\n{ls_coef}')
     if ls_coef.any():
         matrix = np.concatenate((np.ones((system[0].shape[0], 1)), system[0]), axis = 1)
-        print(f'least squares approximation:\n{ls_coef @ system[1]}')
-        print(f'least squares approximation:\n{matrix @ ls_coef @ system[1]}')
-        print(f'least squares approximation directly:\n{least_squares_matrix(system[0]) @ system[1]}')
+        print(f'least squares approximation:\n{matrix @ ls_coef}')
+        print(f'least squares approximation directly:\n{approx_vector}')
+        print(f'error\n{least_squares_approximation_error(system[0], approx_vector)}')
