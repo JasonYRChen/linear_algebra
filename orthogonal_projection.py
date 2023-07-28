2056b8df044bda18e068856fbb0dc99c6c7ecedf
JasonYRChen <jkmylife@gmail.com>
2023-07-29 06:29:49 +0800

    (orthogonal_projection): merge least squares.py into this

--- orthogonal_projection.py
+++ orthogonal_projection.py
@@ -1,51 +1,86 @@
 import numpy as np
 from inverse_matrix import inverse_matrix
 
+"""
+This module is for computing orthogonal projection, or least squares of, a
+vector on a system. The system itself must be linearly independent, or the
+floating error may cause unexpected false.
+"""
 
-def orthogonal_projection_coefficients(matrix):
+def orthogonal_projection_coefficient(matrix, y, need_intercept=False):
     """
-        Return a matrix for computing orthogonal projection coefficients.
-        If column vectors of input matrix is not linearly independent, an
-        empty np.ndarray returns.
+        Return the coeffieicent of projection vector in the matrix of y.
+        If "need_intercept" is True, then an additional 1s vector ([1...1])
+        will be concatenated on the left of the matrix as an intercept. If
+        the matrix is not column-wise linearly independent, an empty
+        np.array returns, or floating error occurs and crashes the result
+        without warnings.
 
-        para:
-          matrix: np.ndarray, a matrix with linearly independent columns.
+        paras:
+          matrix: np.ndarray, the system.
+          y: np.ndarray, an nx1 array to be fitted.
+          need_intercept: bool, need an extra column for intercept or not.
+            If True, an extra 1s column will be concatenated on the left of
+            the matrix.
 
         return:
-          matrix_for_coef: np.ndarray, matrix for computing coefficients.
+          coefficient: np.ndarray, an nx1 array of coefficient.
     """
 
+    if need_intercept:
+        matrix = np.array(matrix)
+        matrix = np.concatenate((np.ones((matrix.shape[0], 1)), matrix), 1)
     inverse_mTm = inverse_matrix(matrix.T @ matrix)
-    if not inverse_mTm.any():
-        return np.array([])
-    matrix_for_coef = inverse_mTm @ matrix.T
-    return matrix_for_coef
+    coefficient = np.array([])
+    if inverse_mTm.any():
+        coefficient = inverse_mTm @ matrix.T @ y
+    return coefficient
 
 
-def orthogonal_projection_matrix_LI(matrix):
+def orthogonal_projection_approximation(matrix, y, need_intercept=False):
     """
-        Return orthogonal projection matrix of a matrix with linearly
-        independent columns. If column vectors of the matrix are not LI,
-        an empty np.ndarray returns.
+        Return the approximation of projection vector in the matrix of y.
+        If "need_intercept" is True, then an additional 1s vector ([1...1])
+        will be concatenated on the left of the matrix as an intercept. If
+        the matrix is not column-wise linearly independent, an empty
+        np.array returns, or floating error occurs and crashes the result
+        without warnings.
 
-        para:
-          matrix: np.ndarray, a matrix with linearly independent columns.
+        paras:
+          matrix: np.ndarray, the system.
+          y: np.ndarray, an nx1 array to be fitted.
+          need_intercept: bool, need an extra column for intercept or not.
+            If True, an extra 1s column will be concatenated on the left of
+            the matrix.
 
         return:
-          op_matrix: np.ndarray, the orthogonal projection matrix.
+          approximation_vector: np.ndarray, an nx1 array of approximation.
+
     """
 
-    matrix_for_coef = orthogonal_projection_coefficients(matrix)
-    if not matrix_for_coef.any():
-        return matrix_for_coef
-    op_matrix = matrix @ matrix_for_coef
-    return op_matrix
+    if need_intercept:
+        matrix = np.array(matrix)
+        matrix = np.concatenate((np.ones((matrix.shape[0], 1)), matrix), 1)
+    coefficient = orthogonal_projection_coefficients(matrix, y)
+    approximation_vector = np.array([])
+    if coefficient.any():
+        approximation_vector = matrix @ coefficient
+    return approximation_vector
 
 
-if __name__ == '__main__':
-    a0 = np.array([[1, -2], [1, 0], [0, 1]])
-    a1 = np.array([[1, 2], [3, 6]])
+def least_squares_approximation_error(target_vector, approx_vector):
+    """
+        Return the error of least squares approximation result, which is 
+        the sum of the squared difference between target vector and
+        approximation vector.
 
-    matrix = a1
-    print(f'matrix:\n{matrix}')
-    print(f'OPM:\n{orthogonal_projection_matrix_LI(matrix)}')
+        paras:
+          target_vector: np.ndarray, the target vector to be approximated.
+          approx_vector: np.ndarray, the approximation vector.
+
+        return:
+          error: float, the approximation error.
+    """
+
+    error = sum((target_vector - approx_vector) ** 2)[0]
+    return error
