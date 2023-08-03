9262d5b4b15af523f0883bac3b8d29146727a3d6
JasonYRChen <jkmylife@gmail.com>
2023-08-03 08:20:34 +0800

    (elementary_row_operation): simplify each function's work

--- elementary_row_operation.py
+++ elementary_row_operation.py
@@ -1,32 +1,6 @@
 import numpy as np
 
 
-__all__ = [
-           'interchange',
-           'scaling',
-           'row_addition'
-          ]
-
-
-def to_ndarray(array):
-    """
-        Convert non-ndarray but array-like array into np.ndarray. If array
-        is np.ndarray, return immediately. The return ndarray is forced to
-        cast to float type.
-
-        para:
-          array: List-like
-
-        return:
-          array: np.ndarray, return a float type matrix.
-    """
-
-    if not isinstance(array, np.ndarray):
-        array = np.array(array)
-    array = array.astype(float)
-    return array
-
-
 def interchange(matrix, row1, row2):
     """
         Return a np.ndarray with designated two rows interchanged.
@@ -39,7 +13,6 @@ def interchange(matrix, row1, row2):
           matrix: np.ndarray, the interchanged matrix.
     """
 
-    matrix = to_ndarray(matrix)
     temp = matrix[row1].copy()
     matrix[row1], matrix[row2] = matrix[row2], temp
     return matrix
@@ -58,7 +31,6 @@ def scaling(matrix, row_index, scalar):
           matrix: np.ndarray.
     """
 
-    matrix = to_ndarray(matrix)
     matrix[row_index] *= scalar
     return matrix
 
@@ -77,13 +49,5 @@ def row_addition(matrix, from_row, scalar, to_row):
           matrix: np.ndarray.
     """
 
-    matrix = to_ndarray(matrix)
     matrix[to_row] += matrix[from_row] * scalar
     return matrix   
-
-
-if __name__ == '__main__':
-    array = [[r*3+c for c in range(1, 4)] for r in range(0, 4)]
-    # array = np.array(array)
-    print(array)
-    print(row_addition(array, 1, 4.0, 2))
